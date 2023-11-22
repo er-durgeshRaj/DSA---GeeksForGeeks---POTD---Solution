788ddef693df3c94571d058769dd0c47e8652a53
Durgesh Raj <142685180+er-durgeshRaj@users.noreply.github.com>
2023-11-22 22:07:56 +0530

    Create Symmetric Tree.cpp

---
+++ Symmetric Tree.cpp
@@ -0,0 +1,54 @@
+// QUESTION
+// Given a Binary Tree. Check whether it is Symmetric or not, i.e. whether the binary tree is a Mirror image of itself or not.
+
+// Example 1:
+
+// Input:
+//          5
+//        /   \
+//       1     1
+//      /       \
+//     2         2
+// Output: 
+// True
+// Explanation: 
+// Tree is mirror image of itself i.e. tree is symmetric
+// Example 2:
+
+// Input:
+//          5
+//        /   \
+//       10     10
+//      /  \     \
+//     20  20     30
+// Output: 
+// False
+// Your Task:
+// You don't need to read input or print anything. Your task is to complete the function isSymmetric() which takes the root of the Binary Tree as its input and returns True if the given Binary Tree is the same as the Mirror image of itself. Else, it returns False.
+
+// Expected Time Complexity: O(N).
+// Expected Auxiliary Space: O(Height of the Tree).
+
+// Constraints:
+// 0<=N<=105
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//SOLUTION
+class Solution{
+    public:
+    // return true/false denoting whether the tree is Symmetric or not
+    bool check(Node* left, Node* right) {
+        if (left == NULL && right == NULL)
+            return true;
+        if (left == NULL || right == NULL || left->data != right->data)
+            return false;
+
+        return check(left->left, right->right) && check(left->right, right->left);
+    }
+
+    bool isSymmetric(Node* root) {
+        if (root == NULL) 
+            return true;
+        return check(root->left, root->right);
+    }
+};
