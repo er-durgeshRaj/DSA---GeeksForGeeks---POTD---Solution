506a0d7af5380d31475078988b827754aaab0361
Durgesh Raj <142685180+er-durgeshRaj@users.noreply.github.com>
2023-12-13 21:59:17 +0530

    Create Consecutive 1's not allowed.cpp

---
+++ Consecutive 1's not allowed.cpp
@@ -0,0 +1,44 @@
+// QUESTION
+// Given a positive integer N, count all possible distinct binary strings of length N such that there are no consecutive 1’s. Output your answer modulo 109 + 7.
+
+// Example 1:
+
+// Input:
+// N = 3
+// Output: 5
+// Explanation:
+// 5 strings are (000,
+// 001, 010, 100, 101).
+// Example 2:
+
+// Input:
+// N = 2
+// Output: 3
+// Explanation: 
+// 3 strings are (00,01,10).
+// Your Task:
+// You don't have to print answer or take inputs. Complete the function countStrings() which takes single integer n, as input parameters and returns an integer denoting the answer. 
+
+// Expected Time Complexity: O(N)
+// Expected Auxiliary Space: O(N)
+
+// Constraints:
+// 1 ≤ N ≤ 105
+// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// SOLUTION
+class Solution{
+public:
+	// #define ll long long
+	 int mod=1e9+7;
+    ll countStrings(int n) {
+        ll f=1;
+        ll s=2;
+        
+        for(int i=2;i<=n;i++){
+            ll t=(f+s)%mod;
+            f=s;
+            s=t;
+        }
+        return s;
+	}
+};
