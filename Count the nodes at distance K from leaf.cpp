dabf6aba134ba177eccf5be0a6df164f119fb50b
Durgesh Raj <142685180+er-durgeshRaj@users.noreply.github.com>
2024-02-06 19:35:50 +0530

    Create Count the nodes at distance K from leaf.cpp

---
+++ Count the nodes at distance K from leaf.cpp
@@ -0,0 +1,83 @@
+// QUESTION
+// Given a binary tree with n nodes and a non-negative integer k, the task is to count the number of special nodes.
+// A node is considered special if there exists at least one leaf in its subtree such that the distance between the node and leaf is exactly k.
+
+// Note: Any such node should be counted only once. For example, if a node is at a distance k from 2 or more leaf nodes, then it would add only 1 to our count.
+
+// Example 1:
+
+// Input:
+// Binary tree
+//         1
+//       /   \
+//      2     3
+//    /  \   /  \
+//   4   5  6    7
+//           \ 
+//           8
+// k = 2
+// Output: 
+// 2
+// Explanation: 
+// There are only two unique nodes that are at a distance of 2 units from the leaf node. (node 3 for leaf with value 8 and node 1 for leaves with values 4, 5 and 7) Note that node 2 isn't considered for leaf with value 8 because it isn't a direct ancestor of node 8.
+// Example 2:
+
+// Input:
+// Binary tree
+//           1
+//          /
+//         3
+//        /
+//       5
+//     /  \
+//    7    8
+//          \
+//           9
+// k = 4
+// Output: 
+// 1
+// Explanation: 
+// Only one node is there which is at a distance of 4 units from the leaf node.(node 1 for leaf with value 9) 
+// Your Task:
+// You don't have to read input or print anything. Complete the function printKDistantfromLeaf() that takes root node and k as inputs and returns the number of nodes that are at distance k from a leaf node. 
+
+// Expected Time Complexity: O(n).
+// Expected Auxiliary Space: O(Height of the Tree).
+
+// Constraints:
+// 1 <= n <= 105
+// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// SOLUTION
+class Solution
+{
+    public:
+    //Function to return count of nodes at a given distance from leaf nodes.
+    int printKDistantfromLeaf(Node* root, int k)
+    {
+    	vector<Node*> path;
+    	unordered_set<Node*> ans;
+    	
+    	function<void(Node *)> dfs = [&](Node * node) {
+    	    if(!node)
+    	        return;
+    	        
+    	    path.push_back(node);
+    	        
+    	    if(!(node -> left) and !(node -> right)){
+    	        int p = (int)path.size() - 1 - k;
+    	        
+    	        if(p > -1)
+    	            ans.insert(path[p]);
+    	    }
+    	    
+    	    dfs(node -> left);
+    	    dfs(node -> right);
+    	    
+    	    path.pop_back();
+    	};
+    	
+    	dfs(root);
+    	
+    	return ans.size();
+    }
+};
